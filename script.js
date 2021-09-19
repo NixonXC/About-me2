824b96ba540cbcca32aef1bdcf85d8b3ac8e87eb
NixonXC <81410474+NixonXC@users.noreply.github.com>
2021-09-20 01:14:18 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,9 @@
+const typed = new Typed(".typing", {
+strings: [
+"🐧 Discord Verified Bot Developer.", 
+"🐔 Web Development Newbie.", 
+"🌌 Html,Css,Js,Python."
+],
+typeSpeed: 75,
+backSpeed: 50,
+});
