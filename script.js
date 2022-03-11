0271df993b6bd6c7364121b8a26c252ce51c4a45
NixonXC <81410474+NixonXC@users.noreply.github.com>
2022-03-11 13:31:07 +0530

    Add files via upload

--- script.js
+++ script.js
@@ -1,9 +1,12 @@
 const typed = new Typed(".typing", {
 strings: [
 "🐧 Verified DIscord Bot Developer", 
-"🐔 Web Development Newbie.", 
-"🌌 Html,Css,Js,Python."
+"💻 I am a Web Developer.",
+"🌌 Html,Css,Js,Python.",
+"🤖 My Discord Bot https://pixiej.xyz/",
+"📕 My 1st Api api.pixiej.xyz",
 ],
 typeSpeed: 75,
 backSpeed: 50,
+loop: true
 });
